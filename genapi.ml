9e1d3379879c5ac65253ae303ae7a0be66a667f4
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2015-11-01 17:10:39 +0100

    Cleanup

--- genapi.ml
+++ genapi.ml
@@ -1,100 +1,14 @@
-let base_url = "https://www.googleapis.com/gmail/v1/users"
+(* module type HTTP_CLIENT = sig *)
+(*   type 'a io *)
 
-module type HTTP_CLIENT = sig
-  type 'a io
+(*   val get : string -> string io *)
+(* end *)
 
-  val get : string -> string io
-end
-
-module type IO = sig
-  type 'a t
-  val bind : 'a t -> ('a -> 'b t) -> 'b t
-  val return : 'a -> 'a t
-end
-
-module V1 (A : sig val token : string end) (IO : IO) (Http : HTTP_CLIENT with type 'a io := 'a IO.t) : sig
-  type 'a io = 'a IO.t
-
-  class type label =
-    object
-      method id : string option
-      method label_list_visibility : [`Label_hide | `Label_show | `Label_show_if_unread] option
-      method message_list_visibility : [`Hide | `Show] option
-      method messages_total : int option
-      method messages_unread : int option
-      method name : string option
-    end
-
-  class type list_labels_response =
-    object
-      method labels : label list
-    end
-
-  class users :
-    object
-      method labels :
-        <
-          list : ?user_id:string -> unit -> list_labels_response io;
-        >
-    end
-end = struct
-  type 'a io = 'a IO.t
-
-  class type label =
-    object
-      method id : string option
-      method label_list_visibility : [`Label_hide | `Label_show | `Label_show_if_unread] option
-      method message_list_visibility : [`Hide | `Show] option
-      method messages_total : int option
-      method messages_unread : int option
-      method name : string option
-    end
-
-  class type list_labels_response =
-    object
-      method labels : label list
-    end
-
-  open Yojson.Basic.Util
-
-  let label_of_json json =
-    object
-      method id = json |> member "id" |> to_string_option
-      method label_list_visibility : [`Label_hide|`Label_show|`Label_show_if_unread] option =
-        match json |> member "labelListVisibility" |> to_string_option with
-        | Some "labelHide" -> Some `Label_hide
-        | Some "labelShow" -> Some `Label_show
-        | Some "labelShowIfUnread" -> Some `Label_show_if_unread
-        | _ -> None
-      method message_list_visibility : [`Hide | `Show] option =
-        match json |> member "messageListVisibility" |> to_string_option with
-        | Some "hide" -> Some `Hide
-        | Some "show" -> Some `Show
-        | _ -> None
-      method messages_total =
-        json |> member "messagesTotal" |> to_int_option
-      method messages_unread =
-        json |> member "messagesUnread" |> to_int_option
-      method name =
-        json |> member "name" |> to_string_option
-    end
-
-  class users =
-    object
-      method labels =
-        object
-          method list ?(user_id = "me") () =
-            let url = Printf.sprintf "%s/%s/labels?access_token=%s" base_url user_id A.token in
-            IO.bind (Http.get url) (fun body ->
-                let json = Yojson.Safe.to_basic (Yojson.Safe.from_string body) in
-                let labels = json |> member "labels" |> to_list |> List.map label_of_json in
-                IO.return (object
-                  method labels = labels
-                end)
-              )
-        end
-    end
-end
+(* module type IO = sig *)
+(*   type 'a t *)
+(*   val bind : 'a t -> ('a -> 'b t) -> 'b t *)
+(*   val return : 'a -> 'a t *)
+(* end *)
 
 let urlencode s =
   let b = Buffer.create 0 in
@@ -161,38 +75,31 @@ end
 (* open Cohttp *)
 (* open Cohttp_lwt_unix *)
 
-(* let server = *)
-(*   let callback _conn req body = *)
-(*     body |> Cohttp_lwt_body.to_string >>= fun body -> Server.respond_string ~status:`OK ~body:"" () *)
+(* let main () = *)
+(*   let module A = Auth (struct *)
+(* let client_id = "921324454177-4gp2pll42hlasklqq7ug0qh2g55up267.apps.googleusercontent.com" *)
+(* let client_secret = "!!SECRET!!" *)
+(* end) *)
 (*   in *)
-(*   Server.create ~mode:(`TCP (`Port 80)) (Server.make ~callback ()) *)
-
-let main () =
-  let module A = Auth (struct
-let client_id = "921324454177-4gp2pll42hlasklqq7ug0qh2g55up267.apps.googleusercontent.com"
-let client_secret = "!!SECRET!!"
-end)
-  in
-  let open Lwt.Infix in
-  let t =
-    A.request_token ["https://mail.google.com"] >>= fun token ->
-    Printf.printf "TOKEN: %s\n%!" token;
-    let module Client =
-    struct
-      module C = Cohttp_lwt_unix.Client
-      let get url =
-        C.get (Uri.of_string url) >>= fun (_, body) -> Cohttp_lwt_body.to_string body
-    end
-    in
-    (* let t = Client.get auth >>= fun _ -> Lwt.return_unit in *)
-    let module V1 = V1 (struct let token = token end) (Lwt) (Client) in
-    let users = new V1.users in
-    users # labels # list () >>= fun labels ->
-    List.iter (fun label -> match label # name with Some label -> Printf.eprintf "LABEL: %s\n%!" label | None -> ()) (labels # labels);
-    Lwt.return_unit
-  in
-  Lwt_unix.run t
-
+(*   let open Lwt.Infix in *)
+(*   let t = *)
+(*     A.request_token ["https://mail.google.com"] >>= fun token -> *)
+(*     Printf.printf "TOKEN: %s\n%!" token; *)
+(*     let module Client = *)
+(*     struct *)
+(*       module C = Cohttp_lwt_unix.Client *)
+(*       let get url = *)
+(*         C.get (Uri.of_string url) >>= fun (_, body) -> Cohttp_lwt_body.to_string body *)
+(*     end *)
+(*     in *)
+(*     (\* let t = Client.get auth >>= fun _ -> Lwt.return_unit in *\) *)
+(*     let module V1 = V1 (struct let token = token end) (Lwt) (Client) in *)
+(*     let users = new V1.users in *)
+(*     users # labels # list () >>= fun labels -> *)
+(*     List.iter (fun label -> match label # name with Some label -> Printf.eprintf "LABEL: %s\n%!" label | None -> ()) (labels # labels); *)
+(*     Lwt.return_unit *)
+(*   in *)
+(*   Lwt_unix.run t *)
 
 module Parser = struct
   open Yojson.Basic.Util
@@ -457,11 +364,12 @@ module Emit = struct
               failwith "emit_query_parameter: unsupported type"
         end
 
-  let emit_query_parameters first url oc parameters =
+  let emit_query_parameters url oc parameters =
     match parameters with
     | [] -> ()
     | (x : parameter) :: xs ->
-        emit_query_parameter first url (pretty x.id) oc x;
+        fprintf oc "Printf.bprintf %s \"?access_token=%%s\" token;\n" url;
+        emit_query_parameter '&' url (pretty x.id) oc x;
         List.iter
           (fun (x : parameter) ->
              emit_query_parameter '&' url (pretty x.id) oc x)
@@ -513,7 +421,7 @@ module Emit = struct
     | s -> ksprintf failwith "http_method: method not supported (%s)" s
 
   let emit_method base_url oc (method_ : method_) =
-    fprintf oc "let %s %a () =\n" (pretty method_.name) emit_parameters method_.parameters;
+    fprintf oc "let %s ?(token = \"\") %a () =\n" (pretty method_.name) emit_parameters method_.parameters;
     fprintf oc "let url = Buffer.create 0 in\n";
     fprintf oc "Printf.bprintf url \"%s\";\n" base_url;
     let query_parameters, path_parameters =
@@ -525,7 +433,7 @@ module Emit = struct
         ) method_.parameters
     in
     emit_path_parameters method_.path "url" oc path_parameters;
-    emit_query_parameters '?' "url" oc query_parameters;
+    emit_query_parameters "url" oc query_parameters;
     fprintf oc "let url = Buffer.contents url in\n";
     fprintf oc "IO.bind (Http.%s url) (fun body ->\n" (http_method method_);
     match method_.response with
@@ -693,7 +601,7 @@ module Emit = struct
         fprintf oc "}\n";
         fprintf oc "end\n"
     | Some _ | None ->
-        ()
+        assert false
 
   let emit_schemas oc schemas =
     let aux = function
@@ -703,7 +611,6 @@ module Emit = struct
           List.iter (fun x -> emit_schema_module "and" oc x) xs
     in
     aux schemas
-    (* List.iter (emit_schema_module oc) schemas *)
 
   let emit oc api =
     fprintf oc "%s\n" prologue;
